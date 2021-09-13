23f0b95255c48d79f189e9cdacb435114a5829bc
Divya Teja Sarraju <divyasajay@divyas-air.lan>
2021-09-13 13:15:52 +0800

    Remove dummy data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import './styles/styles.scss';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
-
-const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 , createdAt:4500}));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
-const expenseThree = store.dispatch(addExpense({ description: 'Rent', amount: 104500}));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 const jsx =(
     <Provider store ={store}>
         <AppRouter/>
